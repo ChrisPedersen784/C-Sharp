44778f6ddb85961de0cf109346e5a7f956539b9d
Christian Pedersen <76157202+ChrisPedersen784@users.noreply.github.com>
2022-09-11 11:28:58 +0200

    Add files via upload

---
+++ StringMirrored.cs
@@ -0,0 +1,29 @@
+﻿namespace programming01
+{
+    internal class Program
+    {
+
+        /*
+         Mirror a string from a user input!
+         */
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a word to be reversed: ");
+            string input = Console.ReadLine();
+
+            string reverse = "";
+
+            Console.WriteLine(input + " " + input.Length + " " + reverse.Length);
+
+            for (int i = input.Length -1; i > -1; i--)
+            {
+                reverse += input[i];
+            }
+
+                Console.Write(reverse);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ doWhile.cs
@@ -0,0 +1,46 @@
+﻿namespace DoWhile
+{
+    internal class Program
+    {
+        /*
+         Creating a do while loop to keep asking the user for the correct input
+         Guess a random number
+         */
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Guess a random integer between 1 - 50");
+            string input;
+
+            int convertInput;
+            Random randomNumber = new Random();
+            int generateRandom = randomNumber.Next(1, 50);
+
+    
+
+            do
+            {
+                Console.Write("Input your guess: ");
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out convertInput))//check for a valid integer 
+                {
+                    Console.WriteLine("Not a valid number");
+                }else if(convertInput > generateRandom)
+                {
+                    Console.WriteLine("Too high, Try again");
+                }else if(convertInput < generateRandom)
+                {
+                    Console.WriteLine("Too low, Try again");
+                }
+
+
+            } while (convertInput != generateRandom); //While convertInput is NOT equal generateRandom Keep running the code
+
+            Console.WriteLine("You guessed my secret number ");
+
+
+            Console.ReadLine();
+        }
+    }
+}
