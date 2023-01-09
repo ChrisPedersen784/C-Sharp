cbd4c969f14aa98d7e73a5750ab055f543b0754b
Christian Pedersen <chrispedersen784@gmail.com>
2023-01-09 12:13:33 +0100

    simple implementation

---
+++ Disaheim/Disaheim/Utility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+
+
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double price;
+            if (amulet.Quality == Level.low)
+            {
+                price = 12.5;
+
+            }
+            else if (amulet.Quality == Level.medium)
+            {
+                price = 20.0;
+            }
+            else
+            {
+                price = 27.5;
+            }
+
+            return price;
+        }
+    }
+}
